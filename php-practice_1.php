caacbfa4721c44dae0d3e28dbcfbf1f4c95022b0
Sayumi Nakagawa <Sayumi.nakagawa25@gmail.com>
2025-09-04 15:39:04 +0900

    修正してプルリクエスト作成

--- php-practice_1.php
+++ php-practice_1.php
@@ -34,12 +34,12 @@ $array = [
     '千葉県',
     '群馬県',
     '茨城県'
-    ];
+        ];
 echo $array[3] . 'と' . $array[4] . 'は関東地方の都道府県です。';
 
 
 // Q7 連想配列-1
-$city = [
+$citys = [
     '東京都' => '新宿区',
     '神奈川県' => '横浜市',
     '千葉県' => '千葉市',
@@ -47,9 +47,9 @@ $city = [
     '栃木県' => '宇都宮市',
     '群馬県' => '前橋市',
     '茨城県' => '水戸市'
-    ];
-foreach ($city as $value) {
-    echo $value. "\n";
+        ];
+foreach ($citys as $city){
+echo $city . "\n";
 }
 
 // Q8 連想配列-2
@@ -61,11 +61,11 @@ $city = [
     '栃木県' => '宇都宮市',
     '群馬県' => '前橋市',
     '茨城県' => '水戸市'
-    ];
+        ];
 $都道府県名 = '埼玉県';
 if($都道府県名 === '埼玉県') {
     echo $都道府県名 . 'の県庁所在地は、' . $city['埼玉県'] . 'です。';
-}
+                            }
 
 // Q9 連想配列-3
 $city = [
@@ -76,15 +76,15 @@ $city = [
     '栃木県' => '宇都宮市',
     '群馬県' => '前橋市',
     '茨城県' => '水戸市'
-    ];
+        ];
 $city['愛知県'] = '名古屋市';
 $city['大阪府'] = '大阪市';
-foreach($city as $ken => $shi)
+foreach($city as $prefecture => $city)
 {
-    if($ken === '東京都' || $ken === '神奈川県' || $ken === '千葉県' || $ken === '埼玉県' || $ken ===  '栃木県' || $ken === '群馬県' || $ken === '茨城県')
-    {echo $ken . 'の県庁所在地は' . $shi . 'です。' . "\n";
-} else {
-    echo $ken . 'は関東地方ではありません。' . "\n";}
+    if($prefecture === '東京都' || $prefecture === '神奈川県' || $prefecture === '千葉県' || $prefecture === '埼玉県' || $prefecture ===  '栃木県' || $prefecture === '群馬県' || $prefecture === '茨城県')
+    {echo $prefecture . 'の県庁所在地は' . $city . 'です。' . "\n";
+    } else {
+    echo $prefecture . 'は関東地方ではありません。' . "\n";}
 }
 
 // Q10 関数-1
@@ -97,20 +97,20 @@ hello('安藤');
 
 // Q11 関数-2
 function calcTaxInPrice($price = 1000)
-{
+ {
     return $price *= 1.1;
-}
+ }
 $price = 1000;
 $taxInPrice = calcTaxInPrice($price);
 echo $price . '円の商品の税込み価格は' . $taxInPrice . '円です。'
 
 // Q12 関数とif文
 function distinguishNum($number) {
-if($number % 2 === 0) {
-    return $number . 'は偶数です。';
-} else {
-    return $number . 'は奇数です。';
-}
+ if($number % 2 === 0) {
+     return $number . 'は偶数です。';
+ } else {
+     return $number . 'は奇数です。';
+ }
 }
 echo distinguishNum(11) . "\n";
 echo distinguishNum(24) . "\n";
